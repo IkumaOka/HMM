ec77fcff5eef55b85f5f36f2ecfec73ea1135a4d
IkumaOka <ikuma1018soccer06@gmail.com>
2020-08-25 11:34:49 +0900

    words_to_id

--- sample_mecab.py
+++ sample_mecab.py
@@ -2,14 +2,26 @@ import MeCab
 
 mecab = MeCab.Tagger()
 
-text = '解析したいテキスト'
-mecab.parse('')#文字列がGCされるのを防ぐ
-node = mecab.parseToNode(text)
-while node:
-    #単語を取得
-    word = node.surface
-    #品詞を取得
-    pos = node.feature.split(",")[1]
-    print('{0} , {1}'.format(word, pos))
-    #次の単語に進める
-    node = node.next
+input_dir = "./datasets/livedoor/dokujo-tsushin/"
+input_data = input_dir + "dokujo-tsushin-4778030.txt"
+
+
+with open(input_data) as f:
+    text = f.read()
+    mecab.parse('') # 文字列がGCされるのを防ぐ
+    node = mecab.parseToNode(text)
+    words = []
+    while node:
+        # 単語を取得
+        word = node.surface
+        words.append(word)
+        # 次の単語に進める
+        node = node.next
+# リスト内の単語の重複を除去
+words = set(words)
+
+# 単語：idの辞書を作成
+words_to_idx = {}
+for i, word in enumerate(words):
+    words_to_idx[word] = i
+
